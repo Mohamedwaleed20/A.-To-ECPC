1603b35a90c620b372e67140567dc3106252f9b2
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-12-12 12:12:57 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+int main()
+{
+    long long x , y , z ;
+    cin >> x >> y >> z ;
+    long long res = min({x,y,z});
+    cout << res ;
+
+    return 0;
+}
